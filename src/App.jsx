0c8e08a19b9c9ecfdadae8b7fa2efe89206ae410
Nikita-Schenikov <nikita-shenikov@mail.ru>
2023-12-11 23:13:58 +0300

    login and register pages

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Register } from "../src/pages/register/register";
+
+function App() {
+  return (
+    <div className="App">
+      <Register></Register>
+    </div>
+  );
+}
+
+export default App;
