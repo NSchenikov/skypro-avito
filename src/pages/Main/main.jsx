dc2caf69c565256cbf3980dea4e4920cc622c413
Nikita-Schenikov <nikita-shenikov@mail.ru>
2023-12-20 23:46:51 +0300

    img still not working

--- src/pages/Main/main.jsx
+++ src/pages/Main/main.jsx
@@ -1,4 +1,4 @@
-import { React, useEffect, useState, useRef } from "react";
+import { React, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getAds } from "../../API/api";
 import "./main.css";
@@ -56,17 +56,19 @@ export const Main = () => {
             <div className="main__content">
               <div className="cards">
                 {ads.map((ad, index) => {
-                  // console.log(
-                  //   `./../../../back-skyVito/data/${ad.images[0].url}`
-                  // );
+                  let imgUrl =
+                    ad.images.length !== 0
+                      ? `/../../../back-skyVito/data/${ad.images[0].url}`
+                      : "";
+                  console.log(imgUrl);
                   return (
                     <div className="cards__item" key={index}>
                       <div className="cards__card card">
                         <div className="card__image">
                           <img
                             alt="pic"
-                            // src={`./../../../back-skyVito/data/${ad.images[0].url}`}
-                            src=""
+                            src="/public/img/ad_images/26bac8a9-5de1-481d-a30b-421c977e659b.png"
+                            // src={imgUrl}
                           />
                         </div>
                         <div className="card__content">
